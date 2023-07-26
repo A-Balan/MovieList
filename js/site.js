08fd908605fa36c8ffb2e6d5ec3f817cb456504f
A-Balan <abalan0326@gmail.com>
2023-07-26 17:57:10 -0400

    clonesd template JS .query selector

--- js/site.js
+++ js/site.js
@@ -1,14 +1,44 @@
-function displayMessage() {
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjdmYzY4ZjQyMTkzZjQ3MzY5YzQ4MmE4ZWUyOTRhYiIsInN1YiI6IjY0YzE2ODgwMDk3YzQ5MDBjNjQzMmZhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tpfGC08xJulq-UYFETixY1zP_Q07lrXjaPUxinVCoYk'
 
-    let inputBox = document.getElementById("message");
+// https://api.themoviedb.org/3/movie/popular
 
-    let message = inputBox.value;
+async function getMovies() {
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: "App Name",
-            text: message
-        }
-    );
+    try {
+        let response = await fetch('https://api.themoviedb.org/3/movie/popular',
+            {
+                headers: {
+                    'Authorization': `Bearer ${API_KEY}`
+                }
+            }
+        );
+
+        let data = await response.json();
+
+        return data;
+
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+async function displayMovies() {
+   
+    const movieListDiv = document.getElementById('movie-list');
+    const moviePosterTemplate = document.getElementById('movie-card-template');
+
+    let data = await getMovies();
+
+    let movies = data.results;
+    // movies is an array of objects
+    movies.forEach(movie => {
+        let movieCard = moviePosterTemplate.content.cloneNode(true);
+
+        let titleElement = movieCard.querySelector('.card-body > h5');
+        titleElement.textContent = movie.title;
+
+        movieListDiv.appendChild(movieCard);
+    });
+
+}
+
